03e800370c854988d385b908449a5da351002da3
Fabian Stiehle <f.stiehle@gmail.com>
2018-04-23 22:31:27 +0200

    many fixes and improvements to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp'),
   browserify = require('browserify'),
+  browserify_css = require("browserify-css"),
   babelify = require('babelify'),
   source = require('vinyl-source-stream'),
   gutil = require('gulp-util'),
@@ -8,30 +9,29 @@ const gulp = require('gulp'),
   minify = require('gulp-minifier'),
   rename = require('gulp-rename'),
   es = require('event-stream'),
-  del = require('del'),
-  runSequence = require('run-sequence');
+  del = require('del')
 
 const DEV_DIR = './gui/src/renderer',
-  DIST_DIR = './gui/dist';
+  DIST_DIR = './gui/dist'
 
 // Gulp tasks
 // ----------------------------------------------------------------------------
 gulp.task('dev-scripts', () => {
   return bundleApp(true);
-});
+})
 
 gulp.task('prod-env', (done) => {
   process.env.NODE_ENV = 'production';
-  done();
-});
+  done()
+})
 
 gulp.task('prod-scripts', () => {
-  return bundleApp(false);
-});
+  return bundleApp(false)
+})
 
 gulp.task('clean', () => {
-  return del([DIST_DIR + '/**/*']);
-});
+  return del([DIST_DIR + '/**/*'])
+})
 
 gulp.task('webserver', (done) => {
   connect.server({
@@ -39,22 +39,24 @@ gulp.task('webserver', (done) => {
     host: 'localhost',
     root: DIST_DIR,
     livereload: true
-  });
-  done();
-});
+  })
+  done()
+})
 
 // Watches for changes in the DEV_DIR
 gulp.task('watch', (done) => {
-  gulp.watch([DEV_DIR + '/**/*'], gulp.parallel('dev-scripts', 'copy-static', 'sass'));
-  done();
-});
+  gulp.watch([DEV_DIR + '/static/**/*'], gulp.task('copy-static'))
+  gulp.watch([DEV_DIR + '/**/*.jsx', DEV_DIR + '/**/*.js'], gulp.task('dev-scripts'))
+  gulp.watch([DEV_DIR + '/sass/**/*.sass'], gulp.task( 'sass'))
+  done()
+})
 
 // Copies assets to the DIST folder
 gulp.task('copy-static', () => {
   return gulp.src(DEV_DIR + '/static/**/*')
     .pipe(gulp.dest(DIST_DIR))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 // Run 'minify' for final shipable code
 gulp.task('minify', () => {
@@ -64,27 +66,27 @@ gulp.task('minify', () => {
     conservativeCollapse: true,
     minifyJS: true,
     minifyCSS: true,
-  })).pipe(gulp.dest(DIST_DIR));
-});
+  })).pipe(gulp.dest(DIST_DIR))
+})
 
 // sass
 gulp.task('sass', () => {
   return gulp.src(DEV_DIR + '/sass/**/*.scss')
-    .pipe(sass({includePaths: ['/sass/', 'node_modules']})
+    .pipe(sass({includePaths: [DEV_DIR + '/sass/']})
     .on('error', sass.logError))
     .pipe(gulp.dest(DIST_DIR + '/css/'))
-    .pipe(connect.reload());
-});
+    .pipe(connect.reload())
+})
 
 // Run this for the final build
-// Run 'minify' before the code is shiped
-gulp.task('build', gulp.parallel('prod-env', 'prod-scripts', 'copy-static', 'sass', 'minify'));
+gulp.task('build', gulp.series('prod-env',
+  gulp.parallel('prod-scripts', 'copy-static', 'sass'), 'minify'))
 
 // Aliase
 gulp.task('default', (done) => {
   gulp.task('build')
   done()
-});
+})
 
 // When running 'gulp' on the terminal this task will fire.
 // It will start watching for changes in every .js file.
@@ -93,7 +95,7 @@ gulp.task('serve', gulp.parallel('dev-scripts',
   'copy-static',
   'sass',
   'webserver',
-  'watch'));
+  'watch'))
 
 // Private Functions
 // ----------------------------------------------------------------------------
@@ -101,7 +103,7 @@ function bundleApp(_debug) {
 
   const files = [
     DEV_DIR + '/renderer.jsx'
-  ];
+  ]
   
   const tasks = files.map((entry) => {
     return browserify({ 
@@ -112,6 +114,7 @@ function bundleApp(_debug) {
     .transform("babelify", { 
       presets: ["env", "react"],
       sourceMaps: true })
+    .transform({global: true}, "browserify-css")
     .bundle()
     .on('error', gutil.log)
     .pipe(source(entry))
@@ -120,10 +123,10 @@ function bundleApp(_debug) {
         dirname: '',
         extname: '.bundle.js'
     }))
-    .pipe(gulp.dest(DIST_DIR))
-    .pipe(connect.reload());
-    });
-  // create a merged stream
-  return es.merge.apply(null, tasks);
-}
+  })
 
+    // create a merged stream
+  return es.merge.apply(null, tasks)
+  .pipe(gulp.dest(DIST_DIR))
+  .pipe(connect.reload())
+}
