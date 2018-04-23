e1fbd39cce514d524b86d636109401d2564d5ff9
Fabian Stiehle <f.stiehle@gmail.com>
2018-04-24 01:09:33 +0200

    set up grid for source view

--- gui/src/renderer/components/Source.jsx
+++ gui/src/renderer/components/Source.jsx
@@ -3,11 +3,11 @@ import Navigation from './Panel/Navigation.jsx'
 
 export default class Layout extends React.Component {
   render() {
-    return <div className="split_view">
-      <div className="side">
+    return <div className="split-view">
+      <div className="col-xs-12 col-md-2">
         <Navigation />
       </div>
-      <div className="content">
+      <div className="col-xs-12 col-md-10">
         Source
       </div>     
     </div>;
