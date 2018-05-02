70a660bd3af0bdb085893c41ca0329b313bc3702
Fabian Stiehle <f.stiehle@gmail.com>
2018-05-02 17:41:46 +0200

    impl uniform api to start tracing

--- tracer.pl
+++ tracer.pl
@@ -1,11 +1,8 @@
 :- encoding(utf8).
 
 :- module(owl_tracer, [ 
-  trace_vars/2,
   op(900, fx, [#, '📌']),
-  (#)/1,
-  trace_constraint/1,
-  trace_labeling/1
+  (#)/1
 ]).
 
 :- use_module(library(clpfd)).
@@ -19,39 +16,63 @@
   to_trace/2,
   constraint/2.
 
-% Trace domain 
-'📌'(X) :- #(X).
-#(X) :- trace_constraint(X).
-trace_constraint(Goal) :-
-  % Todo how to check if it is a constraint operator?
-  ( current_predicate(_, Goal) -> true 
-  ; type_error(predicate_t, Goal)
-  ),
-  call(Goal),
-  term_string(Goal, CId),
-  \+constraint(CId, _),
-  term_variables(Goal, List),
-  maplist(trace_domain(CId), List).
+% Trace Operators
+'📌'(Goal) :- #(Goal).
+'📌'(Goal, Names) :- #(Goal, Names).
+#(Goal) :- #(Goal, Names).
 
-trace_domain(CId, Head) :-
-  fd_var(Head),
-  write(Head).
+#(Goal, Names) :- 
+  nonvar(Goal),
+  tracer(Goal, Names).
+
+% Decide what to trace
+% Trace labeling
+tracer(Goal, Names) :-
+  current_predicate(labeling, Goal), !,
+  write("Trace labeling..."),
+  trace_labeling(Goal, Names).
+
+% Trace Goal
+% Only trace constraint predicates?
+tracer(Goal, Names) :-
+  current_predicate(_, Goal),
+  write("Trace Goal..."),
+  trace_constraint(Goal, Names).
+
+trace_constraint(Goal, Names) :-
+  call(Goal),  
+  term_variables(Goal, Vars),
+  var_names(Vars, Names),
+  assert_constraint(Goal, Names),
+  term_string(Goal, ConstraintID),
+  maplist(trace_var, Vars, Doms),
+  write(Names),
+  write(Vars),
+  write(Doms).
+  % TODO: assert tracepoint
+
+assert_constraint(Goal, Names) :-
+  term_string(Goal, ConstraintID),
+  ( \+constraint(ConstraintID, _) -> true
+  ; permission_error(apply_constraint_to_name, Goal, ConstraintID)
+  ),
+  assertz(constraint(ConstraintID, Names)).
+
+%trace_domain(CId, Head) :-
+%  fd_var(Head),
+%  write(Head).
 
 % Associate Var with name and add to database
-trace_vars(Vars, Names) :-
-  maplist(trace_var, Vars, Names).
+trace_var(Var, Dom) :-
+  write("Trace Var..."),
+  fd_dom(Var, Dom).
 
-trace_var(Var, Name) :-
-  ( fd_var(Var) -> true 
-  ; type_error(fd_var, Var)
-  ),
-  term_string(Var, VarId),
-  \+to_trace(VarId, _),
-  assertz(to_trace(VarId, Name)),
-  trace_unification(Var, Name).
-
-trace_unification(Var, Name) :-
-  when(ground(Var), print_binding(Var, Name)).
+  % TODO: Could've already been grounded!
+%  ( fd_var(Var) -> true 
+%  ; type_error(fd_var, Var)
+ % ),
+  % assert var<->name into database
+ % assert_var(ConstraintID, Var, Name).
 
 print_binding(Var, Name) :- 
   format('new binding for ~w: ~w~n', [Name, Var]).
@@ -59,33 +80,51 @@ print_binding(Var, Name) :-
 print_binding(_, Name) :-
   format('undo binding for ~w~n', [Name]), !, fail.
 
-var_names([], []).
-var_names([Var|T1], [Name|T2]) :-
+var_names(Vars, Names) :-
+  ( nonvar(Names) -> assert_names(Vars, Names)
+  ; get_names(Vars, Names)
+  ).
+
+assert_names(Vars, Names) :-
+  maplist(assert_name, Vars, Names).
+
+assert_name(Var, Name) :-
+  term_string(Var, VarID),
+  ( \+to_trace(VarID, _) -> true
+  ; permission_error(apply_var_to_name, Var, VarID)
+  ),
+  assertz(to_trace(VarID, Name)).
+
+get_names([], []).
+get_names([Var|T1], [Name|T2]) :-
   term_string(Var, VarId),
   to_trace(VarId, Name),
-  var_names(T1, T2).
+  % Throw error when name not found
+  get_names(T1, T2).
 
-trace_labeling(Goal) :-
-  current_predicate(labeling, Goal),
-  Goal =.. [Head, Opts, Vs],
-  var_names(Vs, Names),
-  bagof([Names, Vs, Dom],
-    maplist(trace_labeling(Head, Opts), Vs, Names, Dom),
+trace_labeling(Goal, Names) :-
+  Goal =.. [Head, Opts, Vars],
+  var_names(Vars, Names),
+  bagof([Names, Vars, Dom],
+    maplist(trace_labeling(Head, Opts), Vars, Names, Dom),
     List),
   write(List).
     
 trace_labeling(Head, Opts, Var, Name, Dom) :-
   fd_dom(Var, Dom),
-  print_binding(Dom, Name),
+  % assert trace point, reuse trace_var function OR assert entire list?
+  %print_binding(Dom, Name),
   call(Head, Opts, [Var]).
+
+% TODO: fd_var, ground, etc,
+% Print when not fd_dom but grounded variable
 
 % Quick Tests
 test_trace_vars() :-
   [A,B] ins 0..3,
-  trace_vars([A,B], ["A", "B"]),
   B #> A,
-  trace_labeling(labeling([],[A,B])).
+  '📌'(labeling([],[A,B]), ["A", "B"]).
 
 test_trace_domains() :-
-  '📌'([A,B] ins 0..2),
+  '📌'([A,B] ins 0..2, ["A", "B"]),
   '📌'(A #< B).
