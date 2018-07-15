54254f9adb570bd5326d7ad362f340ffc0460b14
Fabian Stiehle <f.stiehle@gmail.com>
2018-07-15 22:58:44 +0200

    add start screen

--- gui/src/renderer/components/Start.jsx
+++ gui/src/renderer/components/Start.jsx
@@ -15,6 +15,11 @@ export default class Start extends React.Component {
       <div className="col-xs-12 col-md-10 content">
         <div className="border-top">
           <h1 className="text-medium text-gray">Start</h1>
+            <div className="container-medium">            
+              <img width="268px" src="/logo.png" alt="logo" />
+              <h2 className="button button-outlined text-medium"><a target="_blank" href="https://github.com/fstiehle/owl_inspector/blob/master/readme.md">Get started guide</a></h2>
+              <h2 className="button button-outlined text-medium"><a target="_blank" href="https://github.com/fstiehle/owl_inspector/tree/master/examples">Example Prolog files</a></h2>
+            </div>
           </div>
       </div>     
     </div>;
